1509d39a31ab8276bf36b31a4498a80d4f21d2a9
Pedro Henrique Ramos Martins <pedrohrmartins98@gmail.com>
2021-07-27 00:44:57 -0300

    Versão 1.0.0.0 concluída
    Projeto ListaDePedidos funcionando perfeitamente.

---
+++ SumarioDePedidos/SumarioDePedidos/Entities/OrderItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumarioDePedidos.Entities
+{
+    class OrderItem
+    {
+        public int Quantidade { get; set; }
+        public double Price { get; set; }
+        public Product Product { get; set; }
+
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(int quantidade, double price, Product product)
+        {
+            Quantidade = quantidade;
+            Price = price;
+            Product = product;
+        }
+
+        public double SubTotal()
+        {
+            return Quantidade * Price;
+        }
+
+        public override string ToString()
+        {
+            return Product.Name + ", R$" + Price + ", Quantidade: " + Quantidade + ", SubTotal: R$" + SubTotal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ SumarioDePedidos/SumarioDePedidos/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SumarioDePedidos.Entities;
+using SumarioDePedidos.Entities.Enums;
+
+namespace SumarioDePedidos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, email, productName, status;
+            DateTime birthDate;
+            int qtdProduto, qtdItemsInOrder;
+            double productPrice;
+            Client client;
+            Order order;
+
+            Console.Write("Dados do cliente: \n");
+
+            Console.Write("Insira o nome do cliente: ");
+            name = Console.ReadLine();
+
+            Console.Write("Insira o email do cliente: ");
+            email = Console.ReadLine();
+
+            Console.Write("Insira a data de nascimento do cliente: (dd/MM/yyyy HH/mm/ss)");
+            birthDate = DateTime.Parse(Console.ReadLine());
+
+            client = new Client(name, email, birthDate);
+
+            Console.Write("Dados do produto - \n");
+
+            Console.Write("Status do produto: ");
+            status = Console.ReadLine();
+
+
+            order = new Order(DateTime.Now, (OrderStatus) Enum.Parse(typeof(OrderStatus), status), client);
+
+            Console.Write("Quantos produtos terão nessa ordem? ");
+            qtdItemsInOrder = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= qtdItemsInOrder; i++)
+            {
+                Console.Write($"Insira os dados do #{i} produto: \n");
+
+                Console.Write("Nome do Produto: ");
+                productName = Console.ReadLine();
+
+                Console.Write("Preco do Produto: ");
+                productPrice = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.Write("Quantidade do Produto: ");
+                qtdProduto = Convert.ToInt32(Console.ReadLine());
+
+                Product produto = new Product(productName, productPrice);
+                OrderItem item = new OrderItem(qtdProduto, productPrice, produto);
+
+                order.AddItem(item);
+            }
+
+            Console.WriteLine(order.ToString());
+        }
+    }
+}
